80bf45aff0ac63ce3e7c962a0f7942ec084878fe
Scott Walter <scottwalter69@gmail.com>
2026-03-19 15:36:10 -0400

    V0.1.0 GPL 3 tags

--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2026 Scott Walter, MMFP Solutions LLC
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option)
+ * any later version.  See LICENSE for more details.
+ */
+
 package middleware
 
 import (
